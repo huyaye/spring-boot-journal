e1f88a056041547a0674324c552e5b4ae3043706
huyaye <huyaye@naver.com>
2018-04-01 15:49:25 +0900

    Chapter7 data access
    using JPA

--- src/main/java/com/apress/spring/SpringBootJournalApplication.java
+++ src/main/java/com/apress/spring/SpringBootJournalApplication.java
@@ -1,33 +1,25 @@
 package com.apress.spring;
 
-import org.springframework.beans.factory.InitializingBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import com.apress.spring.domain.Journal;
-import com.apress.spring.repository.JournalRepository;
+import com.apress.spring.service.JournalService;
 
 @SpringBootApplication
 public class SpringBootJournalApplication {
+	private static final Logger log = LoggerFactory.getLogger(SpringBootJournalApplication.class);
 
 	@Bean
-	InitializingBean saveData(JournalRepository repo) {
-		return () -> {
-			repo.save(new Journal("스프링 부트 입문", "오늘부터 스프링 부트 공부하기 시작했다", "01/01/2016"));
-			repo.save(new Journal("간단한 스프링 부트 프로젝트", "스프링 부트 프로젝트를 처음 만들어 보았다", "01/02/2016"));
-			repo.save(new Journal("스프링 부트 해부", "스프링 부트를 자세히 살펴보았다", "02/01/2016"));
-			repo.save(new Journal("스프링 부트 클라우드", "클라우드 파운드리를 응용한 스프링 부트를 공부했다", "03/01/2016"));
-		};
-	}
-
-	@Bean
-	CommandLineRunner init(JournalRepository repo) {
+	CommandLineRunner start(JournalService service) {
 		return args -> {
-			System.out.println("---------------------------------");
-			repo.findAll().forEach(System.out::println);
-			System.out.println("---------------------------------");
+			log.info("@@ 데이터 생성....");
+//			service.insertData();
+			log.info("@@ findAll() 호출");
+			service.findAll().forEach(entry -> log.info(entry.toString()));
 		};
 	}
 
